479d157f2bc1a281a05939f740bf9383ba18b7a6
Karen Posada <k100posada@gmail.com>
2017-12-10 12:50:48 -0600

    styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({startLogin}) => (
-    <div>
-        <button onClick={startLogin}>Login </button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+        <h1 className="box-layout__title">Image Blog</h1>
+        <p>Because unsplash is awesome and made with react!</p>
+        <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
+       
     </div>
 );
 
