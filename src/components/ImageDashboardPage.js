245198a68112dc06f6045e02d43646c916025efd
Karen Posada <k100posada@gmail.com>
2017-12-11 19:48:34 -0600

    added a link button to dashboard to link to add image form

--- src/components/ImageDashboardPage.js
+++ src/components/ImageDashboardPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ImagesList from './ImagesList';
 import ImagesListFilters from './ImagesListFilters';
+
 
 const ImageDashboardPage = () => (
     <div>
         <div className="content-container">
+        <div className="page-header">
+        <Link className="button" to="/create">Add New Image</Link>
+        </div>
         <ImagesListFilters />
         </div>
          <ImagesList />
